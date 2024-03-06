aa782bb7a6fa22135e6b868c7f9c9c3a6b9666f9
GustavoBento <gustavo_grb-@hotmail.com>
2024-03-06 20:32:51 -0300

    mostrando somente o video filtrado

--- js/buscarVideos.js
+++ js/buscarVideos.js
@@ -1,4 +1,5 @@
 import { conectaApi } from "./conectaApi.js";
+import constroiCard from "./mostrarVideos.js"
 const botao = document.querySelector("[data-botao]")
 
 async function buscarVideo(evento) {
@@ -6,8 +7,14 @@ async function buscarVideo(evento) {
 
     const dadosDePesquisa = document.querySelector("#pesquisar").value
     const busca = await conectaApi.buscaVideo(dadosDePesquisa)
+    const lista = document.querySelector(".videos__container")
+
+    while (lista.firstChild) {
+        lista.removeChild(lista.firstChild)
+    }
+
+    busca.forEach(elemento => lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
     
-    console.log(busca)
 }
 
 botao.addEventListener("click", evento => buscarVideo(evento))
